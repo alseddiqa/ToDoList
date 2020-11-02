ae5ebab0a2903d2da8beb76f6bb48c84f369ea3a
Abdullah <alseddiq.af@gmail.com>
2020-11-02 15:17:21 +0300

    TaskDetailView
    -Designed the detail task view
    -Added segue from cell to detail

--- ToDoList/ToDoList/TaskDetailsViewController.swift
+++ ToDoList/ToDoList/TaskDetailsViewController.swift
@@ -9,10 +9,23 @@ import UIKit
 
 class TaskDetailsViewController: UIViewController {
 
+    @IBOutlet var taskDatePicker: UIDatePicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        taskDatePicker.minimumDate = Date()
     }
 
-
+    @IBAction func disableDatePicker(_ sender: UISwitch) {
+        
+        //Check the status of the UISwitch
+        if sender.isOn {
+            taskDatePicker.isEnabled = true
+        }
+        else {
+            taskDatePicker.isEnabled = false
+        }
+    }
+    
 }
